9d59cd19833e4ade2289a9c9dc94cb843efd58ca
Shubham Gupta <shubhamg763@gmail.com>
2020-09-01 21:02:41 +0530

    Junit Testing

--- src/test/java/com/wellsfargo/stockmarket/stockexchange/StockExchangeApplicationTests.java
+++ src/test/java/com/wellsfargo/stockmarket/stockexchange/StockExchangeApplicationTests.java
@@ -1,8 +1,12 @@
-package com.wellsfargo.stockexchange;
+package com.wellsfargo.stockmarket.stockexchange;
+
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class StockExchangeApplicationTests {
 
