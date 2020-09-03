ab00eb263f46109a2cce2bbe6d5eda5d118f4724
Shubham Gupta <shubhamg763@gmail.com>
2020-09-03 16:32:14 +0530

    Cross Config

--- src/main/java/com/wellsfargo/stockmarket/stockexchange/controller/StockExchangeController.java
+++ src/main/java/com/wellsfargo/stockmarket/stockexchange/controller/StockExchangeController.java
@@ -6,6 +6,7 @@ import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.wellsfargo.stockmarket.stockexchange.entity.StockExchange;
 import com.wellsfargo.stockmarket.stockexchange.service.StockExchangeService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class StockExchangeController {
 
 	@Autowired
--- src/main/java/com/wellsfargo/stockmarket/stockexchange/entity/StockExchange.java
+++ src/main/java/com/wellsfargo/stockmarket/stockexchange/entity/StockExchange.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 public class StockExchange {
 	
 	@Id 
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	@Column(name = "stockexchangeid")
 	private Integer stockExchangeId;
 	
